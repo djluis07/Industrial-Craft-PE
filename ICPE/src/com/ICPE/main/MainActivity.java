63242fadcf8442cb795114bd4c0d06f77e64676f
ModelPartt <2286108831@qq.com>
2016-11-07 21:09:25 +0800

    fixdd crash

--- ICPE/src/com/ICPE/main/MainActivity.java
+++ ICPE/src/com/ICPE/main/MainActivity.java
@@ -12,7 +12,7 @@ import android.os.*;
 public class MainActivity extends Activity
 {
 	@Override
-	protected void onCreate(Bundle savedInstanceState)
+	public void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main_activity);
