ad4f0e822a3f75323d1e85d93f6c8d75db103085
ModelPartt <2286108831@qq.com>
2016-12-05 22:42:06 +0800

    clean bin

--- ICPELauncher/obj/local/armeabi-v7a/objs/icpe_core/gen/feature/RubTreeFeature.o.d
+++
@@ -1,5 +0,0 @@
-obj/local/armeabi-v7a/objs/icpe_core/gen/feature/RubTreeFeature.o: \
- jni/core/gen/feature/RubTreeFeature.cpp \
- jni/core/gen/feature/RubTreeFeature.h
-
-jni/core/gen/feature/RubTreeFeature.h:
--- ICPELauncher/obj/local/armeabi-v7a/objs/icpe_core/ui/UIRenderer.o.d
+++
@@ -1,4 +0,0 @@
-obj/local/armeabi-v7a/objs/icpe_core/ui/UIRenderer.o: \
- jni/core/ui/UIRenderer.cpp jni/core/ui/UIRenderer.h
-
-jni/core/ui/UIRenderer.h:
